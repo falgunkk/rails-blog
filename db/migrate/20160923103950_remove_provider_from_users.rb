17c272082b93cc074243a26934cda55b572ff9f2
falgun <falgun@qburst.com>
2016-10-17 12:11:21 +0530

    deleted migrtn

--- db/migrate/20160923103950_remove_provider_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveProviderFromUsers < ActiveRecord::Migration[5.0]
-  def up
-    remove_column :users, :provider
-  end
-
-  def down
-    add_column :users, :provider
-  end
-end
--- db/migrate/20160923104302_remove_uid_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUidFromUsers < ActiveRecord::Migration[5.0]
-   def up
-    remove_column :users, :uid,:string
-  end
-
-  def down
-    add_column :users, :uid, :string
-  end
-end
